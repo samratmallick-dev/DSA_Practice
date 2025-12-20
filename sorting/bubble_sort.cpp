21c1d43a05dcb017768b78f472fa0a443cc36ba3
Samrat Mallick <samratmallick832@gmail.com>
2025-12-20 21:33:38 +0530

    Refactor bubble sort implementation to improve clarity and correctness

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n)
+void bubbleSortAscendingOrder(int arr[], int n)
 {
       for (int i = n - 1; i > 0; i--)
       {
-            bool count = 0;
+            bool swapped = false;
             for (int j = 0; j < i; j++)
             {
                   if (arr[j] > arr[j + 1])
                   {
                         swap(arr[j], arr[j + 1]);
-                        count == 1;
+                        swapped = true;
                   }
             }
-            if (count == 0)  break;    
+            if (!swapped)
+                  break;
+      }
+}
+
+void bubbleSortDescendingOrder(int arr[], int n)
+{
+      for (int i = 0; i < n - 1; i++)
+      {
+             bool swapped = false;
+            for (int j = n - 1; j > i; j--)
+            {
+                  if (arr[j - 1] > arr[j])
+                  {
+                        swap(arr[j - 1], arr[j]);
+                        swapped = true;
+                  }
+            }
+            if (!swapped)
+                  break;
       }
 }
 
@@ -29,7 +48,8 @@ int main()
             cin >> arr[i];
       }
 
-      bubbleSort(arr, n);
+      bubbleSortAscendingOrder(arr, n);
+      bubbleSortDescendingOrder(arr, n);
 
       for (int i = 0; i < n; i++)
       {
